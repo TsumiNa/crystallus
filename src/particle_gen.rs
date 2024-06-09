f6395c38f69e5f286fcf2dded011de1a519d527c
TsumiNa <liu.hang.1865@exgmail.com>
2024-06-09 23:56:14 +0900

    update

--- src/particle_gen.rs
+++ src/particle_gen.rs
@@ -1,4 +1,4 @@
-// Copyright 2021 TsumiNa
+// Copyright 2024 TsumiNa
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
